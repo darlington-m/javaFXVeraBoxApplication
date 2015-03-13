90f891cde69b5a66584ff940fd08e16a83bf1e10
darlington-m <mthomoyo@hotmail.com>
2015-03-13 19:48:17 +0000

    Added code to check internet connection

--- src/DataRetrival/ReadingTimer.java
+++ src/DataRetrival/ReadingTimer.java
@@ -1,9 +1,11 @@
 package DataRetrival;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.util.TimerTask;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.UnknownHostException;
 
 //import GUI.VeraGUI;
 
@@ -13,6 +15,32 @@ public class ReadingTimer extends TimerTask {
 	public void run() {
 		// TODO Auto-generated method stub
 		
+		try {
+			InternetConnectionCheck();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void InternetConnectionCheck() throws UnknownHostException,
+			IOException {
+		try {
+			try {
+				URL url = new URL("http://www.csesalford.com");
+				System.out.println(url.getHost());
+				HttpURLConnection con = (HttpURLConnection) url
+						.openConnection();
+				con.connect();
+				if (con.getResponseCode() == 200) {
+					System.out.println("Connection established!!");
+				}
+			} catch (Exception exception) {
+				System.out.println("No Connection");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
